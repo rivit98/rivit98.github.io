2a23ff1285806eaf9006d264b1d6bfe8d4b98a01
rivit98 <albert98g@gmail.com>
2024-11-04 15:49:09 +0100

    improvements

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v $(pwd):/w -w /w -p 1313:13337 --name blog-builder jakejarvis/hugo-extended:latest server --disableFastRender --bind 0.0.0.0 -p 13337
+docker run -it --rm -v $(pwd):/w -w /w -p 1313:13337 --name blog-builder hugomods/hugo:latest server --disableFastRender --bind 0.0.0.0 -p 13337
